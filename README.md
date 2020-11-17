d8b1533cd4689adbecfa0f6485419cc16a668a2c
alexClifton <oooalexoooo@gmail.com>
2020-11-16 20:13:59 -0800

    install older node-sass to link properly wtih material-ui

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # shadowrun
 - material ui
   - classNames on custom components dont work.
-    - 
+    - need atleast 2 layers deep of specificity.
   - grid
   - textfield
   - box
--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import './App.css';
+import './App.scss';
 import Button from '@material-ui/core/Button';
 
 function App() {
   return (
     <div className="App">
       <h1>xsjiadowrun</h1>
-      <Button variant="contained" color="primary">
+      <Button className="override" variant="contained" color="primary">
         <span>BEGIN</span>
       </Button>
     </div>
