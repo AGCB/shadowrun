9fc018144e525be2abb9ad76f9bf4d59a9cdf5ce
alexClifton <oooalexoooo@gmail.com>
2022-08-13 23:49:27 -0700

    update header

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
 import type { NextPage } from "next";
 
 const Home: NextPage = () => {
-  return <div className="text-2xl text-red-200">HOME PAssaGE</div>;
+  return <div className="text-2xl text-red-200">shadowrun v0.0.0</div>;
 };
 
 export default Home;
